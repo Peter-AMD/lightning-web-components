fa035d0bd8fe3dd6ac354e9642ff0ac5d712aa29
gpeteamd <gpeteamd@gmail.com>
2022-04-07 19:02:00 +0800

    fix: format

--- src/modules/example/hello/__tests__/hello.test.js
+++ src/modules/example/hello/__tests__/hello.test.js
@@ -2,37 +2,37 @@ import { createElement } from 'lwc';
 import Hello from 'example/hello';
 
 describe('hello', () => {
-    afterEach(() => {
-        // The jsdom instance is shared across test cases in a single file so reset the DOM
-        while (document.body.firstChild) {
-            document.body.removeChild(document.body.firstChild);
-        }
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  it('displays greeting specified by input', () => {
+    const EXPECTED = 'Test';
+
+    // Create element
+    const element = createElement('example-hello', {
+      is: Hello
     });
+    document.body.appendChild(element);
 
-    it('displays greeting specified by input', () => {
-        const EXPECTED = 'Test';
+    // Verify default greeting
+    let h1 = element.shadowRoot.querySelector('h1');
+    expect(h1.textContent).not.toBe(`Hello, ${EXPECTED}!`);
 
-        // Create element
-        const element = createElement('example-hello', {
-            is: Hello
-        });
-        document.body.appendChild(element);
+    // Trigger new greeting
+    const inputEl = element.shadowRoot.querySelector('input');
+    inputEl.value = EXPECTED;
+    inputEl.dispatchEvent(new CustomEvent('input'));
 
-        // Verify default greeting
-        let h1 = element.shadowRoot.querySelector('h1');
-        expect(h1.textContent).not.toBe(`Hello, ${EXPECTED}!`);
-
-        // Trigger new greeting
-        const inputEl = element.shadowRoot.querySelector('input');
-        inputEl.value = EXPECTED;
-        inputEl.dispatchEvent(new CustomEvent('input'));
-
-        // Return a promise to wait for any asynchronous DOM updates. Jest
-        // will automatically wait for the Promise chain to complete before
-        // ending the test and fail the test if the promise rejects.
-        return Promise.resolve().then(() => {
-            // Verify displayed greeting
-            expect(h1.textContent).toBe(`Hello, ${EXPECTED}!`);
-        });
+    // Return a promise to wait for any asynchronous DOM updates. Jest
+    // will automatically wait for the Promise chain to complete before
+    // ending the test and fail the test if the promise rejects.
+    return Promise.resolve().then(() => {
+      // Verify displayed greeting
+      expect(h1.textContent).toBe(`Hello, ${EXPECTED}!`);
     });
+  });
 });
